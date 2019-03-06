3ab43e6f6a3823807795541cb701510dd242729e
Amanda D. Clark <adc0032@auburn.edu>
2019-03-05 23:04:58 -0600

    Added string finding function and for loop usage with two output examples

---
+++ part_1/for_do_done_CLARK.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Enter Search Word for All Files" searchall
+
+touch $searchall.search.txt
+
+for file in ~/homework_6/part_2/book_pages/*
+do
+    ~/homework_6/part_1/string_finder_CLARK.sh $file $searchall >> $searchall.search.txt
+done
+
---
+++ part_1/string_finder_CLARK.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+
+string_finder () {
+
+linenum=$(grep -i -n "$2" $1 |cut -d : -f 1|sed ':a;N;$!ba;s/\n/ /g')
+count=$(grep -i -c "$2" $1)
+filename=$1
+
+echo -------------------------------------------------
+echo
+echo "Searching Filename: $filename"
+echo
+echo "There were $count matching lines with the search word"
+echo
+echo "The search word was found on the following lines: "
+echo "$linenum"
+}
+
+string_finder $1 $2
